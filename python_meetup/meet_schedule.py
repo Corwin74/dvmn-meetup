5f27f38981919e346ddeab03311999bd215bf6ea
Alexander Samokhin <samokhin@shockland.ru>
2022-08-03 22:46:36 +0300

    add schedule draft

--- python_meetup/meet_schedule.py
+++ python_meetup/meet_schedule.py
@@ -3,7 +3,6 @@ from telegram.ext import CallbackContext
 
 
 def show_program(update: Update, context: CallbackContext):
-    print(update)
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
@@ -11,3 +10,8 @@ def show_program(update: Update, context: CallbackContext):
     query.answer()
 
     query.edit_message_text(text=f"Selected option: {query.data}")
+    context.bot.send_message(
+            chat_id=update.callback_query.from_user.id,
+            text='Программа мероприятий:\n1. Спикер: Вася Пупкин "Телеграмм-боты"\n2. Нетворкинг'
+        )
+    return 'START'
